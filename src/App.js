d435c80939f77f57266965ad5ef4bfa88f7d70d3
suntax777 <43720142+suntax777@users.noreply.github.com>
2022-01-09 11:06:49 +0900

    Section4-15

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./styles.css";
 import { InlineStyle } from "./components/InlineStyle";
 import { CssModules } from "./components/CssModules";
 import { StyledJsx } from "./components/StyledJsx";
+import { StyledComponents } from "./components/StyledComponents";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <InlineStyle />
       <CssModules />
       <StyledJsx />
+      <StyledComponents />
     </div>
   );
 }
